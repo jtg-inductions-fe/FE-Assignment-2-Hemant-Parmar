51e66aead0e597b970f5c2566ba079e976566979
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-09 13:54:08 +0530

    HP_A2_05: start ceation of 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,47 @@
+import { Link } from 'react-router-dom';
+
+import { Box, Button, Container, Typography } from '@mui/material';
+
+import NotFOundImageURL from '@assets/images/404.svg';
+
 export const NotFound = () => (
-    <h1>
-        This is the Page Not Found Page, <em>404!</em>
-    </h1>
+    <Container
+        maxWidth="xl"
+        sx={{
+            textAlign: 'center',
+        }}
+    >
+        <img height={600} src={NotFOundImageURL} alt="Not Found" />
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 4,
+                alignItems: 'center',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h1">Page not Found</Typography>
+                <Typography variant="body2">
+                    Oops! Looks like you followed a bad link. If you think this
+                    is a problem with us, please tell us.
+                </Typography>
+            </Box>
+            <Button
+                component={Link}
+                to="/"
+                variant="contained"
+                sx={{ textTransform: 'none', borderRadius: 3 }}
+            >
+                Go back home
+            </Button>
+        </Box>
+    </Container>
 );
