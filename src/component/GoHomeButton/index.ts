2e660853caf6242cd2fd7e47751a6185fad3917e
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-11 02:59:15 +0530

    HP_A2_05: create reusable fallback container and improve styles

---
+++ src/component/GoHomeButton/index.ts
@@ -0,0 +1,1 @@
+export { GoHomeButton } from './GoHomeButton.component';
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export { SearchBar } from './SearchBar';
 export { NavAccordion } from './NavAccordion';
 export { CustomListItem } from './CustomListItem';
 export { NavButton } from './NavButton';
+export { GoHomeButton } from './GoHomeButton';
 
 export type {NavAccordionProps} from './NavAccordion';
 export type {NavButtonProps} from './NavButton';
