b1a6e9150d83dd6553c2ead6437b3aab60934b2d
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-18 20:31:31 +0530

    HP_A2_07: create initial graph

--- src/pages/Overview/Overview.page.tsx
+++ src/pages/Overview/Overview.page.tsx
@@ -1,9 +1,11 @@
 import { Box } from '@mui/material';
 
 import { Gallery } from '@containers';
+import { Graph } from '@containers';
 
 export const Overview = () => (
     <Box>
         <Gallery />
+        <Graph />
     </Box>
 );
