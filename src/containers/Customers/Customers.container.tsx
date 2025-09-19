793fff62718fa55f4527bad5463d54902683e019
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-20 01:04:30 +0530

    HP_A2_08: create common customers and products section

--- src/containers/Customers/Customers.container.tsx
+++ src/containers/Customers/Customers.container.tsx
@@ -1,13 +1,22 @@
-import { CustomTable } from '@components';
-
+import { CustomContainer, CustomTable } from '@components';
 import { Grid2 } from '@mui/material';
 import { latestCustomers } from '@data';
+import type { RowData } from '@components';
 
+const data: RowData[] = latestCustomers.map((user) => ({
+    content: user.name,
+    caption: user.email,
+    avatar: user.avatar,
+    value: user.money,
+    format: (val: number) => `$${val}`,
+}));
 
 export const Customers = () => {
     return (
-        <Grid2 size={{ xs: 12, sm: 3 }}>
-            <CustomTable users={latestCustomers} heading='Latest Customers'/>
+        <Grid2 size={{ xs: 12, sm: 4 }}>
+            <CustomContainer heading="Latest Customers" disableGutters>
+                <CustomTable users={data} />
+            </CustomContainer>
         </Grid2>
     );
 };
