f32f7f00d90d59daf70bdbcdbb385b6a63ad7de9
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-19 22:41:28 +0530

    HP_A2_07: create a reusable custom container

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export { Profile } from './Profile';
 export { SearchBar } from './SearchBar';
 export { PrimaryButton } from './PrimaryButton';
 export { CustomTooltip } from './CustomTooltip';
+export { CustomContainer } from './CustomContainer';
 
 export type { NavAccordionProps } from './NavAccordion';
 export type { NavButtonProps } from './NavButton';
