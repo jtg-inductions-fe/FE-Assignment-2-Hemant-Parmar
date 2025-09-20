7618435eeaca864aca82a8857c81fcca5fbb60c0
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-20 17:12:21 +0530

    HP_A2_09: customize products section

--- src/containers/Products/Products.container.tsx
+++ src/containers/Products/Products.container.tsx
@@ -7,13 +7,20 @@ const data: RowData[] = topProducts.map((product) => ({
     content: product.label,
     caption: product.technology,
     value: product.sales,
-    format: (val: number) => <Typography>{val} sales</Typography>,
+    format: (val: number) => (
+        <>
+            {val}
+            <Typography component="span" color="text.secondary">
+                &nbsp;sales
+            </Typography>
+        </>
+    ),
 }));
 
 export const Products = () => {
     return (
         <Grid2 size={{ xs: 12, md: 6, lg: 8 }}>
-            <CustomContainer heading="Latest Customers" disableGutters>
+            <CustomContainer heading="Top Products" disableGutters>
                 <CustomTable data={data} />
             </CustomContainer>
         </Grid2>
