463327e41393b70593cb5feab90bb678576dfd99
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-20 15:57:12 +0530

    HP_A2_08: improve spacing and add separator

--- src/components/CustomTable/CustomTable.component.tsx
+++ src/components/CustomTable/CustomTable.component.tsx
@@ -10,35 +10,45 @@ import {
 
 import { CustomTableProps } from './CustomTable.types';
 
-export const CustomTable = ({ users }: CustomTableProps) => {
+export const CustomTable = ({ data }: CustomTableProps) => {
     return (
         <TableContainer>
             <Table>
                 <TableBody>
-                    {users.map((user, index) => (
-                        <TableRow key={index}>
-                            {'avatar' in user ? (
+                    {data.map((item, index) => (
+                        <TableRow
+                            key={index}
+                            sx={{
+                                '& td': {
+                                    borderBottom: (theme) =>
+                                        index < data.length - 1
+                                            ? `1px solid ${theme.palette.divider}`
+                                            : null,
+                                },
+                            }}
+                        >
+                            {'avatar' in item ? (
                                 <TableCell padding="none">
                                     <Avatar
-                                        src={user.avatar}
-                                        alt={user.content}
+                                        src={item.avatar}
+                                        alt={item.content}
                                     />
                                 </TableCell>
                             ) : null}
                             <TableCell padding="none">
-                                <Typography variant="h3" component="p">
-                                    {user.content}
+                                <Typography variant="h3">
+                                    {item.content}
                                 </Typography>
                                 <Typography
                                     variant="caption"
                                     color="text.secondary"
                                 >
-                                    {user.caption}
+                                    {item.caption}
                                 </Typography>
                             </TableCell>
                             <TableCell>
-                                <Typography variant="h3" component="p">
-                                    {user.format(user.value)}
+                                <Typography variant="h3" textAlign="right">
+                                    {item.format(item.value)}
                                 </Typography>
                             </TableCell>
                         </TableRow>
