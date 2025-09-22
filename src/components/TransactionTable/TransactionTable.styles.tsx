530104251ff8532c86ef06d3c3ba1110621d3645
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-22 15:00:08 +0530

    HP_A2_10: improve style and add formatter

--- src/components/TransactionTable/TransactionTable.styles.tsx
+++ src/components/TransactionTable/TransactionTable.styles.tsx
@@ -1,27 +1,27 @@
-import { Chip, useTheme } from '@mui/material';
+import { Chip, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { Transaction } from './TransactionTable.types';
 
-export const StatusChip = ({ label }: { label: Transaction['status'] }) => {
-    const theme = useTheme();
+export const StatusChip = styled(Chip)<{ label: Transaction['status'] }>(
+    ({ theme, label }) => ({
+        backgroundColor:
+            label === 'Completed'
+                ? theme.palette.success.main
+                : label === 'In Progress'
+                  ? theme.palette.info.main
+                  : theme.palette.error.main,
+        color:
+            label === 'Completed'
+                ? theme.palette.success.contrastText
+                : label === 'In Progress'
+                  ? theme.palette.info.contrastText
+                  : theme.palette.error.contrastText,
+        ...theme.typography.caption,
+    }),
+);
 
-    return (
-        <Chip
-            label={label}
-            size='small'
-            sx={{
-                backgroundColor:
-                    label === 'Completed'
-                        ? theme.palette.success.main
-                        : label === 'In Progress'
-                          ? theme.palette.info.main
-                          : theme.palette.error.main,
-                color:
-                    label === 'Completed'
-                        ? theme.palette.success.contrastText
-                        : label === 'In Progress'
-                          ? theme.palette.info.contrastText
-                          : theme.palette.error.contrastText,
-            }}
-        />
-    );
-};
+export const HeadCellText = styled(Typography)(({ theme }) => ({
+    fontSize: theme.typography.subtitle1.fontSize,
+    fontWeight: theme.typography.fontWeightMedium,
+    color: theme.palette.text.secondary,
+}));
