ca2bb45e212a8c3f645f7690dffcef25447dda6a
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-24 16:57:59 +0530

    HP_A2_06: add alt to error image

--- src/pages/ErrorPage/ErrorPage.page.tsx
+++ src/pages/ErrorPage/ErrorPage.page.tsx
@@ -7,5 +7,6 @@ export const ErrorPage = () => (
         imageURL={ErrorImageURL}
         title="Something has gone seriously wrong"
         desc="It’s always time for a coffee break We should be back by the time you finish your coffee."
+        alt="Error illustration"
     />
 );
