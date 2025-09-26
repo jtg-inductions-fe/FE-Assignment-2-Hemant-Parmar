0bc65f6517b6d4c86a4e6e90530ef5d76ac2aa1b
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-26 16:28:33 +0530

    HP_A2_09: format and add product type

--- src/data/topProducts.ts
+++ src/data/topProducts.ts
@@ -1,5 +1,12 @@
+export type Product = {
+    label: string;
+    route: string;
+    technology: string;
+    sales: number;
+};
+
 // The List of top products provided by Themesberg
-export const topProducts = [
+export const topProducts: Product[] = [
     {
         label: 'Restaurant Booking App',
         route: 'restaurant-booking-app',
