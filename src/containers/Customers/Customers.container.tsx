87e1ced94b1047184b7b8da0f34b40870b885945
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-26 16:02:56 +0530

    HP_A2_08: format and add types

--- src/containers/Customers/Customers.container.tsx
+++ src/containers/Customers/Customers.container.tsx
@@ -1,22 +1,21 @@
-import { CustomContainer, CustomTable } from '@components';
 import { Grid2 } from '@mui/material';
-import { latestCustomers } from '@data';
+
 import type { RowData } from '@components';
+import { CustomContainer, CustomTable } from '@components';
+import { latestCustomers } from '@data';
 
 const data: RowData[] = latestCustomers.map((user) => ({
     content: user.name,
     caption: user.email,
     avatar: user.avatar,
-    value: user.money,
+    value: user.amount,
     format: (val: number) => `$${val}`,
 }));
 
-export const Customers = () => {
-    return (
-        <Grid2 size={{ xs: 12, md: 6, lg: 4 }}>
-            <CustomContainer heading="Latest Customers" disableGutters>
-                <CustomTable data={data} />
-            </CustomContainer>
-        </Grid2>
-    );
-};
+export const Customers = () => (
+    <Grid2 size={{ xs: 12, md: 6, lg: 4 }}>
+        <CustomContainer heading="Latest Customers" disableGutters>
+            <CustomTable data={data} />
+        </CustomContainer>
+    </Grid2>
+);
