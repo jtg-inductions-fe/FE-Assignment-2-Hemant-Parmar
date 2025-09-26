fc50e1710758ed98797e845dcf3b4c3378ac23fe
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-26 21:05:42 +0530

    HP_A2_10: make builtin formatter for transactions

--- src/components/ReusableTable/ReusableTable.types.ts
+++ src/components/ReusableTable/ReusableTable.types.ts
@@ -1,7 +1,10 @@
+import { ReactNode } from 'react';
+
 export type Column<T> = {
-    key: keyof T | string;
+    key: keyof T;
+    category?: string; // If category is assigned, the formatter will be ignored
     label: string;
-    render?: (row: T) => React.ReactNode;
+    formatter?: (data: T) => ReactNode;
     hideOnMobile?: boolean;
 };
 
@@ -9,3 +12,5 @@ export type ReusableTableProps<T> = {
     columns: Column<T>[];
     data: T[];
 };
+
+export type Status = 'Completed' | 'In Progress' | 'Cancelled';
