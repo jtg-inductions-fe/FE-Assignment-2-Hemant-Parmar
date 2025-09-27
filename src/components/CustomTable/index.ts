8a5717b42f94a69fd335f1ffd99a325f1f5eb44d
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-27 14:48:25 +0530

    HP_A2_08: edit Table import and add section label

--- src/components/CustomTable/index.ts
+++
@@ -1,2 +0,0 @@
-export { CustomTable } from './CustomTable.component';
-export type { RowData } from './CustomTable.types';
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,8 +6,8 @@ export { Profile } from './Profile';
 export { SearchBar } from './SearchBar';
 export { PrimaryButton } from './PrimaryButton';
 export { Container } from './Container';
-export { CustomTable } from './CustomTable';
+export { Table } from './Table';
 
 export type { NavAccordionProps } from './NavAccordion';
 export type { NavButtonProps } from './NavButton';
-export type { RowData } from './CustomTable';
+export type { RowData } from './Table';
