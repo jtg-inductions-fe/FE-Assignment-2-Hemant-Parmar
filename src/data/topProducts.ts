e8824522d2c75838fc41e5f89df848fbb14f5822
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-27 18:29:57 +0530

    HP_A2_09: remove type from product data file

--- src/data/topProducts.ts
+++ src/data/topProducts.ts
@@ -1,12 +1,5 @@
-export type Product = {
-    label: string;
-    route: string;
-    technology: string;
-    sales: number;
-};
-
 // The List of top products provided by Themesberg
-export const topProducts: Product[] = [
+export const topProducts = [
     {
         label: 'Restaurant Booking App',
         route: 'restaurant-booking-app',
