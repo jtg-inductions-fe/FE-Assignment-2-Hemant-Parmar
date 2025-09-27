8d194c82dfdfde16781a00f1c71f220dc9e537a1
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-27 17:13:10 +0530

    HP_A2_11: create a semantic footer with proper styles

--- src/data/footerConfig.ts
+++ src/data/footerConfig.ts
@@ -1,9 +1,20 @@
-import FacebookIconURL from '@assets/images/icons/FacebookIcon.svg';
-import TwitterIconURL from '@assets/images/icons/TwitterIcon.svg';
-import GithubIconURL from '@assets/images/icons/GithubIcon.svg';
 import DribbleIconURL from '@assets/images/icons/DribbleIcon.svg';
+import FacebookIconURL from '@assets/images/icons/FacebookIcon.svg';
+import GithubIconURL from '@assets/images/icons/GithubIcon.svg';
+import TwitterIconURL from '@assets/images/icons/TwitterIcon.svg';
 
-export const footerConfig = {
+export type FooterLink = {
+    url: string;
+    iconURL: string;
+    ariaLabel: string;
+};
+
+export type FooterConfig = {
+    description: string;
+    links: FooterLink[];
+};
+
+export const footerConfig: FooterConfig = {
     description: '&copy; 2021 Themesberg, LLC. All rights reserved.',
     links: [
         {
