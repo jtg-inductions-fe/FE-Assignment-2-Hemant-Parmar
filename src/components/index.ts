42461f0f5732700f79685a610aeacda3f2313a46
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-09-27 13:18:28 +0530

    HP_A2_07: rename container & tooltip and improve semantics

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,8 +5,7 @@ export { NavButton } from './NavButton';
 export { Profile } from './Profile';
 export { SearchBar } from './SearchBar';
 export { PrimaryButton } from './PrimaryButton';
-export { CustomTooltip } from './CustomTooltip';
-export { CustomContainer } from './CustomContainer';
+export { Container } from './Container';
 
 export type { NavAccordionProps } from './NavAccordion';
 export type { NavButtonProps } from './NavButton';
